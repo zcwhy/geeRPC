307a853e736132286ac56b064b20bbb4f4536ac9
zcwhy <845562684@qq.com>
2025-06-29 16:34:23 +0800

    滑动窗口添加单元测试 & 实现代码bugfix

--- rollingwindow.go
+++ rollingwindow.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -23,29 +22,32 @@ func NewRollingWindow(size int, interval time.Duration) *RollingWindow {
 	return &RollingWindow{
 		size:     size,
 		buckets:  make([]Bucket, size),
-		offset:   -1,
-		interval: interval,
+		offset:   0,
+		interval: interval * time.Millisecond,
 		lastTime: time.Now(),
 	}
 }
 
 // 距离上次最后一次更新window过去了n个span，所以如果这n个span中如果有数据，一定是过期的数据
 func (rw *RollingWindow) span() int {
+	//since := time.Since(rw.lastTime)
+	//fmt.Println(since, rw.interval, since/rw.interval)
 	return min(int(time.Since(rw.lastTime)/rw.interval), rw.size)
 }
 
 func (rw *RollingWindow) Add(totalCnt, successCnt int) {
 	nSpan := rw.span()
-	fmt.Println(nSpan)
+	//fmt.Println(nSpan)
 
 	for i := 0; i < nSpan; i++ {
-		rw.buckets[rw.offset+1+i%rw.size].reset()
+		rw.buckets[(rw.offset+1+i)%rw.size].reset()
 	}
 
-	rw.buckets[rw.offset+nSpan].totalCnt = totalCnt
-	rw.buckets[rw.offset+nSpan].successCnt = successCnt
+	newOff := rw.offset + nSpan%rw.size
+	rw.buckets[newOff].totalCnt += totalCnt
+	rw.buckets[newOff].successCnt += successCnt
 
-	rw.offset += nSpan
+	rw.offset = newOff
 	rw.lastTime = time.Now()
 }
 
--- rollingwindow_test.go
+++ rollingwindow_test.go
@@ -3,19 +3,57 @@ package main
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestRollingWindow(t *testing.T) {
-	window := NewRollingWindow(5, 1000)
+	window := NewRollingWindow(5, 50)
 
 	window.Add(1, 1)
 	curTotal, curSucc := window.Reduce()
-	assert.Equal(t, curTotal, 1)
-	assert.Equal(t, curSucc, 1)
+	assert.Equal(t, 1, curTotal)
+	assert.Equal(t, 1, curSucc)
 
 	window.Add(1, 0)
 	curTotal, curSucc = window.Reduce()
-	assert.Equal(t, curTotal, 2)
-	assert.Equal(t, curSucc, 1)
+	assert.Equal(t, 2, curTotal)
+	assert.Equal(t, 1, curSucc)
 
+	time.Sleep(time.Millisecond * 50)
+	window.Add(1, 1)
+	curTotal, curSucc = window.Reduce()
+	assert.Equal(t, 3, curTotal)
+	assert.Equal(t, 2, curSucc)
+
+	time.Sleep(time.Millisecond * 300)
+	window.Add(1, 1)
+	curTotal, curSucc = window.Reduce()
+	assert.Equal(t, 1, curTotal)
+	assert.Equal(t, 1, curSucc)
+}
+
+func TestNSpan(t *testing.T) {
+	window := NewRollingWindow(5, 50)
+
+	window.Add(1, 1)
+	assert.Equal(t, 0, window.span())
+	assert.Equal(t, 0, window.offset)
+
+	time.Sleep(time.Millisecond * 50)
+	assert.Equal(t, 1, window.span())
+
+	window.Add(1, 1)
+	assert.Equal(t, 1, window.offset)
+
+	time.Sleep(time.Millisecond * 100)
+	assert.Equal(t, 2, window.span())
+
+	window.Add(1, 1)
+	assert.Equal(t, 3, window.offset)
+
+	time.Sleep(time.Millisecond * 300)
+	assert.Equal(t, 5, window.span())
+
+	window.Add(1, 1)
+	assert.Equal(t, 3, window.offset)
 }
